2061f04ce79e5123b96cdc85f182d943c3527730
HaiPham <pthai076@icloud.com>
2022-07-31 20:11:49 +0700

    [add] docker config

---
+++ src/github.com/boilerplate/scripts/entrypoint.dev.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+
+echo "#################### migrating dev db"
+go run cmd/dbmigrate/main.go
+
+echo "#################### migrating test db"
+go run cmd/dbmigrate/main.go -dbname=boilerplatetest
+
+echo "#################### downloading CompileDaemon"
+# disable go modules to avoid this package from getting into go.mod
+# as we only need it locally to watch and rebuild server on change
+GO111MODULE=off go get github.com/githubnemo/CompileDaemon
+
+echo "#################### starting deamon"
+CompileDaemon --build="go build -o main cmd/api/main.go" --command=./main
