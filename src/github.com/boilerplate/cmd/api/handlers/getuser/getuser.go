de6b498aecf6f7a1ab9408e1f203aee5e8e1d48d
HaiPham <pthai076@icloud.com>
2022-08-19 01:35:53 +0700

    [add] user in middleware

--- src/github.com/boilerplate/cmd/api/handlers/getuser/getuser.go
+++ src/github.com/boilerplate/cmd/api/handlers/getuser/getuser.go
@@ -1,15 +1,49 @@
 package getuser
 
 import (
+	"database/sql"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"net/http"
 
+	"github.com/boilerplate/cmd/api/handlers/models"
 	"github.com/boilerplate/pkg/application"
+	"github.com/boilerplate/pkg/middleware"
 	"github.com/julienschmidt/httprouter"
 )
 
 func Do(app *application.Application) httprouter.Handle {
+	mdw := []middleware.Middleware{
+		middleware.LogRequest,
+		validRequest,
+	}
+
+	return middleware.Chain(getUser(app), mdw...)
+}
+
+func getUser(app *application.Application) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
-		fmt.Fprintf(w, "hello")
+		// fmt.Fprintf(w, "hello")
+		defer r.Body.Close()
+
+		id := r.Context().Value(models.CtxKey("userid"))
+		user := &models.User{ID: id.(int)}
+
+		if err := user.GetByID(r.Context(), app); err != nil {
+			if errors.Is(err, sql.ErrNoRows) {
+				w.WriteHeader(http.StatusPreconditionFailed)
+				fmt.Fprintf(w, "user does not exist")
+				return
+			}
+
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Oops")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(user)
+		w.Write(response)
 	}
 }
