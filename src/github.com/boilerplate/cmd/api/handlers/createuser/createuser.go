c375a14f5271c7e3ea4f375d6535f424ae4f0fd9
HaiPham <pthai076@icloud.com>
2022-08-19 02:05:38 +0700

    [add] post user

---
+++ src/github.com/boilerplate/cmd/api/handlers/createuser/createuser.go
@@ -0,0 +1,35 @@
+package createuser
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/boilerplate/cmd/api/handlers/models"
+	"github.com/boilerplate/pkg/application"
+	"github.com/boilerplate/pkg/middleware"
+	"github.com/julienschmidt/httprouter"
+)
+
+func Do(app *application.Application) httprouter.Handle {
+	return middleware.Chain(createUser(app), middleware.LogRequest)
+}
+
+func createUser(app *application.Application) httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
+		defer r.Body.Close()
+
+		user := &models.User{}
+		json.NewDecoder(r.Body).Decode(user)
+
+		if err := user.Create(r.Context(), app); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprintf(w, "Oops")
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		response, _ := json.Marshal(user)
+		w.Write(response)
+	}
+}
--- src/github.com/boilerplate/cmd/api/router/router.go
+++ src/github.com/boilerplate/cmd/api/router/router.go
@@ -1,6 +1,7 @@
 package router
 
 import (
+	"github.com/boilerplate/cmd/api/handlers/createuser"
 	"github.com/boilerplate/cmd/api/handlers/getuser"
 	"github.com/boilerplate/pkg/application"
 	"github.com/julienschmidt/httprouter"
@@ -9,5 +10,6 @@ import (
 func Get(app *application.Application) *httprouter.Router {
 	mux := httprouter.New()
 	mux.GET("/users/:id", getuser.Do(app))
+	mux.POST("/users", createuser.Do(app))
 	return mux
 }
